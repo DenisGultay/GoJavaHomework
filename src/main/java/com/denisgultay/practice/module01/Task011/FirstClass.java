4e8394a1689e47463965799a2dcd0949785d8f7a
Denis Gultay <denisgultay@gmail.com>
2017-01-30 22:49:57 +0200

    Initial commit

---
+++ src/main/java/com/denisgultay/practice/module01/Task011/FirstClass.java
@@ -0,0 +1,14 @@
+package com.denisgultay.practice.module01.Task011;
+
+/**
+ * Created by Користувач on 30.01.2017.
+ */
+public class FirstClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("My first program on Java...");
+
+    }
+
+}
---
+++ src/main/java/com/denisgultay/practice/module01/Task011/FourthClass.java
@@ -0,0 +1,13 @@
+package com.denisgultay.practice.module01.Task011;
+
+/**
+ * Created by Користувач on 30.01.2017.
+ */
+public class FourthClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("My first program on Java...");
+
+    }
+}

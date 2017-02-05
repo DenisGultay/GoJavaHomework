489b5f515f4be200d127226e6f96ba648de06c68
Denis Gultay <denisgultay@gmail.com>
2017-02-05 22:03:01 +0200

    Переробив task022

--- src/main/java/com/denisgultay/practice/module02/task022/BankTransaction.java
+++ src/main/java/com/denisgultay/practice/module02/task022/BankTransaction.java
@@ -6,29 +6,18 @@ package com.denisgultay.practice.module02.task022;
 public class BankTransaction {
 
     public static void main(String[] args) {
-
         BankTransaction transaction = new BankTransaction();
-
         transaction.singleTransaction(500, 480);
-
         transaction.singleTransaction(1500, 300);
-
-
     }
 
     public void singleTransaction(int balance, int withdrawal) {
-
-        double commision = withdrawal * 0.05;
-
-        double newBalance = balance - withdrawal - commision;
-
-        if ((withdrawal + commision) <= balance) {
-
-            System.out.println("OK " + commision + " " + newBalance);
-
-        } else
+        double commisionOfTransaction = withdrawal * 0.05;
+        double newBalance = balance - withdrawal - commisionOfTransaction;
+        if ((withdrawal + commisionOfTransaction) <= balance) {
+            System.out.println("OK " + commisionOfTransaction + " " + newBalance);
+        } else {
             System.out.println("NO");
-
-
+        }
     }
 }

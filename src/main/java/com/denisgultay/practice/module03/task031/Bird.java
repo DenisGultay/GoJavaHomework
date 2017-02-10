ec62037c87b14a766d9150d28554951c52e1922e
Denis Gultay <denisgultay@gmail.com>
2017-02-11 00:56:28 +0200

    module03 task031 додаткове завдання

--- src/main/java/com/denisgultay/practice/module03/task031/Bird.java
+++ src/main/java/com/denisgultay/practice/module03/task031/Bird.java
@@ -2,17 +2,38 @@ package com.denisgultay.practice.module03.task031;
 
 public class Bird {
 
+    private String line1 = "I am walking";
+    private String line2 = "I am flying";
+    private String line3 = "I am singing";
+    private String line4 = "I am Bird";
+
+    private String name;
+
     public static void main(String[] args) {
 
-        Bird myBird = new Bird();
-        myBird.someoneSing("I am");
+        Bird bird = new Bird();
+        bird.someoneSing();
+
+        Bird birdGovorun = new Bird("Govorun");
+        birdGovorun.someoneSing();
+
     }
 
-    public void someoneSing(String a) {
+    public Bird() {
+    }
 
-        System.out.println(a + " walking");
-        System.out.println(a + " flying");
-        System.out.println(a + " singing");
-        System.out.println(a + " Bird");
+    public Bird(String name) {
+        this.name = name;
+    }
+
+    public void someoneSing() {
+
+        if (name != null) {
+            System.out.println("I am " + name);
+        }
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
     }
 }

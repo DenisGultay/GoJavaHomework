bdeb800e6ddbc25d3b57675017f4be34c54c05d6
Denis Gultay <denisgultay@gmail.com>
2017-02-11 22:16:32 +0200

    module03 task031 додаткове завдання (переробив)

--- src/main/java/com/denisgultay/practice/module03/task031/Bird.java
+++ src/main/java/com/denisgultay/practice/module03/task031/Bird.java
@@ -2,11 +2,9 @@ package com.denisgultay.practice.module03.task031;
 
 public class Bird {
 
-    private String line1 = "I am walking";
-    private String line2 = "I am flying";
-    private String line3 = "I am singing";
-    private String line4 = "I am Bird";
+    enum IAmDoingSomething {walking, flying, singing, Bird}
 
+    public static final String I_AM = "I am ";
     private String name;
 
     public static void main(String[] args) {
@@ -16,7 +14,6 @@ public class Bird {
 
         Bird birdGovorun = new Bird("Govorun");
         birdGovorun.someoneSing();
-
     }
 
     public Bird() {
@@ -27,13 +24,12 @@ public class Bird {
     }
 
     public void someoneSing() {
-
         if (name != null) {
             System.out.println("I am " + name);
         }
-        System.out.println(line1);
-        System.out.println(line2);
-        System.out.println(line3);
-        System.out.println(line4);
+        System.out.println(I_AM + IAmDoingSomething.walking);
+        System.out.println(I_AM + IAmDoingSomething.flying);
+        System.out.println(I_AM + IAmDoingSomething.singing);
+        System.out.println(I_AM + IAmDoingSomething.Bird);
     }
 }

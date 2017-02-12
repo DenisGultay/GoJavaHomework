e72cd7ce587294dc085791cc671fc5481c9783ab
Denis Gultay <denisgultay@gmail.com>
2017-02-12 13:55:27 +0200

    module03 task031 Створив константу классу I_AM

--- src/main/java/com/denisgultay/practice/module03/task031/Bird.java
+++ src/main/java/com/denisgultay/practice/module03/task031/Bird.java
@@ -2,9 +2,8 @@ package com.denisgultay.practice.module03.task031;
 
 public class Bird {
 
-    enum IAmDoingSomething {walking, flying, singing, Bird}
 
-    public static final String I_AM = "I am ";
+    public static final String I_AM = " I am walking\n" + " I am flying\n" + " I am singing\n" + " I am Bird\n";
     private String name;
 
     public static void main(String[] args) {
@@ -25,11 +24,8 @@ public class Bird {
 
     public void someoneSing() {
         if (name != null) {
-            System.out.println("I am " + name);
+            System.out.println(" I am " + name);
         }
-        System.out.println(I_AM + IAmDoingSomething.walking);
-        System.out.println(I_AM + IAmDoingSomething.flying);
-        System.out.println(I_AM + IAmDoingSomething.singing);
-        System.out.println(I_AM + IAmDoingSomething.Bird);
+        System.out.println(I_AM);
     }
 }

c627a00b1f7c4df8d8d30c3d6365434c6dc5f1a8
Denis Gultay <denisgultay@gmail.com>
2017-03-29 23:35:22 +0300

    module: 07 продовження

---
+++ src/main/java/com/denisgultay/practice/module07/task072/ListMain.java
@@ -0,0 +1,51 @@
+package com.denisgultay.practice.module07.task072;
+
+import com.denisgultay.practice.module04.task042.Currency;
+import com.denisgultay.practice.module07.task071.Order;
+import com.denisgultay.practice.module07.task071.User;
+import java.util.Set;
+import java.util.TreeSet;
+
+import java.util.*;
+
+public class ListMain {
+
+    public static void main(String[] args) {
+
+        User Antonov = new User(11L, "Anton", "Antonov", "Kyiv", 5000);
+        User Denisov = new User(22L, "Denis", "Denisov", "Lviv", 5500);
+        User Danilov = new User(33L, "Danil", "Danilov", "Dnipro", 6000);
+        User Borisov = new User(44L, "Boris", "Borisov", "Kyiv", 7000);
+        User Petrov = new User(55L, "Petro", "Petrov", "Lviv", 5000);
+        User Kirilov = new User(66L, "Kiril", "Kirilov", "Kyiv", 6500);
+        User Pavlov = new User(77L, "Pavlo", "Pavlov", "Poltava", 4000);
+        User Viktorov = new User(88L, "Viktor", "Viktorov", "Rivne", 7000);
+        User Myhailov = new User(99L, "Myhailo", "Myhailov", "Kyiv", 5500);
+        User Bohdanov = new User(111L, "Bohdan", "Bohdanov", "Lviv", 5000);
+
+
+        List<Order> orderList = new ArrayList<Order>();
+        orderList.add(new Order(101L, 10, Currency.USD, "Insomnia", "KSD", Bohdanov));
+        orderList.add(new Order(102L, 20, Currency.EUR, "Christine", "ABG", Antonov));
+        orderList.add(new Order(103L, 40, Currency.UAH, "Misery", "KSD", Viktorov));
+        orderList.add(new Order(104L, 20, Currency.USD, "Green Mile", "FOL", Danilov));
+        orderList.add(new Order(105L, 40, Currency.UAH, "Dreamcatcher", "KSD", Pavlov));
+        orderList.add(new Order(106L, 10, Currency.EUR, "Revival", "ABG", Borisov));
+        orderList.add(new Order(107L, 50, Currency.UAH, "Green Mile", "FOL", Myhailov));
+        orderList.add(new Order(108L, 50, Currency.UAH, "Desperation", "KSD", Denisov));
+        orderList.add(new Order(109L, 10, Currency.USD, "Insomnia", "KSD", Petrov));
+        orderList.add(new Order(110L, 30, Currency.UAH, "It", "AST", Kirilov));
+
+        Collections.sort(orderList);
+        System.out.println(orderList);
+        System.out.println();
+        Collections.sort(orderList, Order.PriceAndCityComp);
+        System.out.println(orderList);
+        System.out.println();
+        Collections.sort(orderList,Order.NameIdCityComp);
+        System.out.println(orderList);
+
+    }
+
+
+}
---
+++ src/main/java/com/denisgultay/practice/module07/task073/SetMain.java
@@ -0,0 +1,75 @@
+package com.denisgultay.practice.module07.task073;
+
+
+import com.denisgultay.practice.module04.task042.Currency;
+import com.denisgultay.practice.module07.task071.Order;
+import com.denisgultay.practice.module07.task071.User;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SetMain {
+
+    public static void main(String[] args) {
+
+        User Willis = new User(11L, "Bruce", "Willis", "Washington", 50000);
+        User Depp = new User(22L, "Johny", "Depp", "Chicago", 55500);
+        User DeNiro = new User(33L, "Robert", "DeNiro", "Los-Angeles", 67000);
+        User Smith = new User(44L, "Will", "Smith", "Denver", 75000);
+        User Pitt = new User(55L, "Brad", "Pitt", "New-York", 85000);
+        User Damond = new User(66L, "Mett", "Damond", "Philadelphia", 60500);
+        User Statham = new User(77L, "Jason", "Statham", "San-Francisco", 54000);
+        User Rives = new User(88L, "Kianu", "Rives", "Huston", 47000);
+        User Clooney = new User(99L, "George", "Clooney", "Las-Vegas", 5500);
+        User Bale = new User(111L, "Cristian", "Bale", "Boston", 5000);
+
+
+        Set<Order> orderSet = new TreeSet<>();
+        orderSet.add(new Order(101L, 10, Currency.USD, "Insomnia", "KSD", Willis));
+        orderSet.add(new Order(102L, 20, Currency.EUR, "Christine", "ABG", Damond));
+        orderSet.add(new Order(103L, 40, Currency.UAH, "Misery", "KSD", Smith));
+        orderSet.add(new Order(104L, 20, Currency.USD, "Green Mile", "FOL", Bale));
+        orderSet.add(new Order(105L, 40, Currency.UAH, "Dreamcatcher", "KSD", Clooney));
+        orderSet.add(new Order(106L, 10, Currency.EUR, "Revival", "ABG", Depp));
+        orderSet.add(new Order(107L, 50, Currency.UAH, "Stand", "FOL", Statham));
+        orderSet.add(new Order(108L, 10, Currency.USD, "Mr.Mersedes", "KSD", Rives));
+        orderSet.add(new Order(104L, 20, Currency.USD, "Green Mile", "FOL", Bale));
+        orderSet.add(new Order(110L, 30, Currency.UAH, "It", "AST", Pitt));
+
+        //   System.out.println(orderByUserLastName((TreeSet<Order>) orderSet, "Petrov"));
+        //     System.out.println(maxPriceOrder((TreeSet<Order>) orderSet));
+        System.out.println(nonDollarOrder((TreeSet<Order>) orderSet));
+    }
+
+    public static boolean orderByUserLastName(TreeSet<Order> treeSetOrders, String lastName) {
+        for (Order order : treeSetOrders) {
+            if (order.getUser().getLastName().equals(lastName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static Order maxPriceOrder(TreeSet<Order> orders) {
+
+        Order maxPriceOrder = orders.last();
+        for (Order order : orders) {
+            if (order.getPrice() > orders.last().getPrice()) {
+                maxPriceOrder = order;
+                break;
+            }
+        }
+        return maxPriceOrder;
+    }
+
+    public static TreeSet<Order> nonDollarOrder(TreeSet<Order> orders) {
+        Iterator<Order> iterator = orders.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().getCurrency().equals(Currency.USD)) {
+                iterator.remove();
+            }
+        }
+        return orders;
+    }
+}
